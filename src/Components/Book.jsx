0a1f4bb2e7efefcddf2b24689f2d26ea9ac87c7a
RAGHUNATH A.G <119778016+Ragu27ag@users.noreply.github.com>
2023-09-12 09:29:14 +0530

    Update Book.jsx

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import backendinstance from "../Axios/axios";
 import { Link, useNavigate } from "react-router-dom";
 import { Box, Button, Typography } from "@mui/material";
@@ -7,45 +7,66 @@ import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import Loading from "./Loading";
 
-
 const Book = () => {
-  const user = JSON.parse(localStorage.getItem("user"));
-  // const { no } = useSelector((state) => state.bookreducer);
+  const user = React.useMemo(
+    () => JSON.parse(localStorage.getItem("user")) || {},
+    []
+  ); // const { no } = useSelector((state) => state.bookreducer);
   // localStorage.setItem("no", no);
   const val = JSON.parse(localStorage.getItem("reduxno"));
 
   // const busno = localStorage.getItem("no");
-    const busno = val.bookreducer.no;
+  const busno = val.bookreducer.no;
 
   const [bookdata, setbookdata] = useState("");
-  const [tickets, setTickets] = useState("");
-  // const arr = [];
-    const [intervalcount, setintervalcount] = useState(0);
+  // const [tickets, setTickets] = useState("");
+  const arr = [];
+  const [intervalcount, setintervalcount] = useState(0);
+  let history = useMemo(() => [], []);
 
+  const setDiable = useCallback(() => {
+    history?.forEach((his) => {
+      document.getElementById(his).disabled = true;
+      document.getElementById(his).style.backgroundColor = "#e93e3e";
+    });
+  }, [history]);
 
   const getData = useCallback(async () => {
     // console.log(no);
     const { data } = await backendinstance.get(`/tickets/${busno}`);
 
+    const bookeddata = await backendinstance.get(
+      `/tickets/bookhistory/${busno}`
+    );
+
+    bookeddata.data.map((dat) => history.push(...dat.count));
+    console.log(history);
+    console.log(bookeddata.data);
     setbookdata(data);
-  }, [busno]);
+    setDiable();
+  }, [busno, history, setDiable]);
 
   const navigate = useNavigate();
 
   // const mem = useMemo(() => getData(), [getData]);
 
   useEffect(() => {
-    getData();
-     return () => {
-      // console.log(intervalcount);
-      clearInterval(intervalcount);
-      // console.log("cleared");
-    };
-  }, [getData,intervalcount]);
+    if (Object.keys(user).length === 0) {
+      console.log("if");
+      navigate("/login");
+    } else {
+      getData();
+      return () => {
+        // console.log(intervalcount);
+        clearInterval(intervalcount);
+        // console.log("cleared");
+      };
+    }
+  }, [getData, intervalcount, user, navigate]);
 
   // console.log(bookdata);
 
-  // arr.push(bookdata);
+  arr.push(bookdata);
   // console.log(arr);
 
   const [open, setOpen] = React.useState(false);
@@ -75,53 +96,71 @@ const Book = () => {
     </React.Fragment>
   );
 
+  const [seat, setSeat] = useState([]);
+  const seatsLeft1 = ["1", "2", "3", "4", "5"];
+  const seatsLeft2 = ["10", "9", "8", "7", "6"];
+  const seatsRight = ["11", "12", "13", "14", "15"];
+  const handleSeats = (sno) => {
+    console.log(seat);
+    console.log(sno);
+    let chk = seat.includes(sno);
+    console.log(chk);
+    if (chk) {
+      let re = seat.filter((s) => s !== sno);
+      console.log(re);
+      setSeat(re);
+      document.getElementById(sno).style.backgroundColor = "white";
+    } else {
+      setSeat([...seat, sno]);
+      document.getElementById(sno).style.backgroundColor = "grey";
+    }
+
+    // document.getElementById(sno).disabled = true;
+  };
+
+  console.log(seat);
   const handleticket = async () => {
     // console.log(tickets);
-    const currtic = bookdata.available - tickets;
+    if (seat.length === 0) {
+      alert("Select Seats");
+    } else {
+      document.getElementById("bookbutt").disabled = true;
+      const currtic = bookdata.available - seat.length;
+      const ticobj = {
+        ...bookdata,
+        available: currtic,
+      };
 
+      const history = {
+        ...ticobj,
+        name: user.name,
+        count: seat,
+        email: user.email,
+      };
 
-    if(tickets > bookdata.available){
-      alert('Enter valid no')
+      const response1 = await backendinstance.put("/tickets", ticobj);
+
+      const response2 = await backendinstance.post(
+        "/tickets/bookhistory",
+        history
+      );
+
+      // console.log(response1, response2);
+
+      //const response3 =  await backendinstance.post("/email", history);
+
+      // console.log(ticobj);
+
+      if (response1 && response2) {
+        localStorage.removeItem("no");
+        handleClick();
+        const timecount = setTimeout(() => {
+          // console.log(timecount);
+          navigate("/");
+        }, 2000);
+        setintervalcount(timecount);
+      }
     }
-    else{
-        const ticobj = {
-      ...bookdata,
-      available: currtic,
-    };
-
-    const history = {
-      ...ticobj,
-      name: user.name,
-      count: tickets,
-      email: user.email,
-    };
-
-    const response1 = await backendinstance.put("/tickets", ticobj);
-
-    const response2 = await backendinstance.post(
-      "/tickets/bookhistory",
-      history
-    );
-
-    // console.log(response1, response2);
-
-    //const response3 =  await backendinstance.post("/email", history);
-
-    // console.log(ticobj);
-    localStorage.removeItem("no");
-    handleClick();
-    if (response1 && response2) {
-      handleClick();
-      const timecount = setTimeout(() => {
-        // console.log(timecount);
-        navigate("/");
-      }, 4000);
-      setintervalcount(timecount);
-    }
-      
-    }
-
-  
   };
   return (
     <>
@@ -137,16 +176,22 @@ const Book = () => {
       <div
         style={{
           display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          paddingTop: "50px",
+          flexWrap: "wrap",
+          justifyContent: "space-evenly",
         }}
       >
-        {bookdata === "" ? (
-        <Loading />
-        ) : (
-          <>
-            {/* {" "}
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            paddingTop: "50px",
+          }}
+        >
+          {bookdata === "" ? (
+            <Loading />
+          ) : (
+            <>
+              {/* {" "}
           <p>Bus no : {bookdata.no}</p>
           <p>Form : {bookdata.from}</p> <p>To : {bookdata.to}</p>
           <p>Data : {bookdata.date}</p>
@@ -161,20 +206,20 @@ const Book = () => {
             id="nooftic"
             onChange={(e) => setTickets(e.target.value)}
           /> */}
-            <Box
-              sx={{
-                border: "1px solid grey",
-                width: "300px",
-                height: "350px",
-                textAlign: "center",
-                borderLeft: "2px solid #e93e3e",
-                borderRadius: "8px",
-                boxShadow:
-                  "  rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
-              }}
-              margin={2}
-            >
-              {/* <p
+              <Box
+                sx={{
+                  border: "1px solid grey",
+                  width: "300px",
+                  height: "350px",
+                  textAlign: "center",
+                  borderLeft: "2px solid #e93e3e",
+                  borderRadius: "8px",
+                  boxShadow:
+                    "  rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+                }}
+                margin={2}
+              >
+                {/* <p
               style={{
                 padding: "2px",
                 textOverflow: "ellipsis",
@@ -187,106 +232,280 @@ const Book = () => {
                 {h.name.toUpperCase()}
               </span>
             </p> */}
-              <p
-                style={{
-                  padding: "2px",
-                  textOverflow: "ellipsis",
-                  color: "grey",
-                  flexWrap: "nowrap",
-                }}
-              >
-                Bus no :{" "}
-                <span style={{ color: "black", fontFamily: "serif" }}>
-                  {bookdata.no.toUpperCase()}
-                </span>
-              </p>
-              <p
-                style={{
-                  padding: "2px",
-                  textOverflow: "ellipsis",
-                  color: "grey",
-                  whiteSpace: "nowrap",
-                  overflow: "hidden",
-                }}
-              >
-                From :{" "}
-                <span style={{ color: "black", fontFamily: "serif" }}>
-                  {bookdata.from.toUpperCase()}
-                </span>
-              </p>
-              <p
-                style={{
-                  padding: "2px",
-                  textOverflow: "ellipsis",
-                  color: "grey",
-                  flexWrap: "nowrap",
-                  whiteSpace: "nowrap",
-                  overflow: "hidden",
-                }}
-              >
-                To :{" "}
-                <span style={{ color: "black", fontFamily: "serif" }}>
-                  {bookdata.to.toUpperCase()}
-                </span>
-              </p>
-              <p
-                style={{
-                  padding: "2px",
-                  textOverflow: "ellipsis",
-                  color: "grey",
-                }}
-              >
-                Date :{" "}
-                <span style={{ color: "black", fontFamily: "serif" }}>
-                  {bookdata.date}
-                </span>
-              </p>
-              <p
-                style={{
-                  padding: "2px",
-                  textOverflow: "ellipsis",
-                  color: "grey",
-                }}
-              >
-                Time :{" "}
-                <span style={{ color: "black", fontFamily: "serif" }}>
-                  {" "}
-                  {bookdata.time}
-                </span>
-              </p>
-              <p
-                style={{
-                  padding: "2px",
-                  textOverflow: "ellipsis",
-                  color: "grey",
-                }}
-              >
-                Available Ticket :{" "}
-                <span style={{ color: "black", fontFamily: "serif" }}>
-                  {" "}
-                  {bookdata.available}
-                </span>
-              </p>
+                <p
+                  style={{
+                    padding: "2px",
+                    textOverflow: "ellipsis",
+                    color: "grey",
+                    flexWrap: "nowrap",
+                  }}
+                >
+                  Bus no :{" "}
+                  <span style={{ color: "black", fontFamily: "serif" }}>
+                    {bookdata?.no?.toUpperCase()}
+                  </span>
+                </p>
+                <p
+                  style={{
+                    padding: "2px",
+                    textOverflow: "ellipsis",
+                    color: "grey",
+                    whiteSpace: "nowrap",
+                    overflow: "hidden",
+                  }}
+                >
+                  From :{" "}
+                  <span style={{ color: "black", fontFamily: "serif" }}>
+                    {bookdata.from.toUpperCase()}
+                  </span>
+                </p>
+                <p
+                  style={{
+                    padding: "2px",
+                    textOverflow: "ellipsis",
+                    color: "grey",
+                    flexWrap: "nowrap",
+                    whiteSpace: "nowrap",
+                    overflow: "hidden",
+                  }}
+                >
+                  To :{" "}
+                  <span style={{ color: "black", fontFamily: "serif" }}>
+                    {bookdata.to.toUpperCase()}
+                  </span>
+                </p>
+                <p
+                  style={{
+                    padding: "2px",
+                    textOverflow: "ellipsis",
+                    color: "grey",
+                  }}
+                >
+                  Date :{" "}
+                  <span style={{ color: "black", fontFamily: "serif" }}>
+                    {bookdata.date}
+                  </span>
+                </p>
+                <p
+                  style={{
+                    padding: "2px",
+                    textOverflow: "ellipsis",
+                    color: "grey",
+                  }}
+                >
+                  Time :{" "}
+                  <span style={{ color: "black", fontFamily: "serif" }}>
+                    {" "}
+                    {bookdata.time}
+                  </span>
+                </p>
+                <p
+                  style={{
+                    padding: "2px",
+                    textOverflow: "ellipsis",
+                    color: "grey",
+                  }}
+                >
+                  Available Ticket :{" "}
+                  <span style={{ color: "black", fontFamily: "serif" }}>
+                    {" "}
+                    {bookdata.available}
+                  </span>
+                </p>
 
-              <input
+                {/* <input
                 type="number"
                 name="nooftic"
                 id="nooftic"
                 placeholder="Enter no of tickets"
                 onChange={(e) => setTickets(e.target.value)}
-              />
-              <br />
-              <br />
-              <Button
-                variant="contained"
-                sx={{ backgroundColor: "#e93e3e" }}
-                onClick={handleticket}
+              />*/}
+                <span>Seats : </span>
+                {seat.map((val) => (
+                  <span>{val + " "}</span>
+                ))}
+                <br />
+                <br />
+                <Button
+                  variant="contained"
+                  sx={{ backgroundColor: "#e93e3e" }}
+                  onClick={handleticket}
+                  id="bookbutt"
+                >
+                  Confirm Booking
+                </Button>
+              </Box>
+            </>
+          )}
+        </div>
+
+        <Box
+          sx={{
+            border: "2px solid blue",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "200px",
+            marginTop: "50px",
+            height: "400px",
+            position: "relative",
+          }}
+        >
+          <div
+            style={{
+              border: "2px solid",
+              position: "absolute",
+              height: "20px",
+              width: "14px",
+              borderRadius: "5px",
+              top: "140px",
+              left: "15px",
+            }}
+          ></div>
+          <div
+            style={{
+              border: "2px solid",
+              position: "absolute",
+              height: "20px",
+              width: "14px",
+              borderRadius: "5px",
+              top: "140px",
+              right: "15px",
+            }}
+          ></div>
+          <div
+            style={{
+              border: "2px solid",
+              position: "absolute",
+              height: "20px",
+              width: "14px",
+              borderRadius: "5px",
+              top: "250px",
+              left: "15px",
+            }}
+          ></div>
+          <div
+            style={{
+              border: "2px solid",
+              position: "absolute",
+              height: "20px",
+              width: "14px",
+              borderRadius: "5px",
+              top: "250px",
+              right: "15px",
+            }}
+          ></div>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "150px",
+              border: "2px solid",
+              borderRadius: "3px",
+              // marginTop: "90px",
+              height: "250px",
+              backgroundColor: "#d6d6d6",
+              // paddingTop: "50px",
+            }}
+          >
+            <div
+              style={{
+                marginRight: "5px",
+              }}
+            >
+              {seatsLeft1.map((no) => (
+                <button
+                  id={no}
+                  style={{
+                    backgroundColor: "white",
+                    display: "block",
+                    marginTop: "10px",
+                    borderRadius: "5px",
+                  }}
+                  onClick={() => handleSeats(no)}
+                >
+                  {no}
+                </button>
+              ))}
+            </div>
+            <div
+              style={{
+                marginRight: "20px",
+              }}
+            >
+              {seatsLeft2.map((no) => (
+                <button
+                  id={no}
+                  style={{
+                    backgroundColor: "white",
+                    display: "block",
+                    marginTop: "10px",
+                    borderRadius: "5px",
+                  }}
+                  onClick={() => handleSeats(no)}
+                >
+                  {no}
+                </button>
+              ))}
+            </div>
+
+            <div style={{}}>
+              <div
+                style={{
+                  border: "1px solid",
+                  height: "10px",
+                  width: "100px",
+                  marginLeft: "10px",
+                  position: "absolute",
+                  top: "68px",
+                  left: "40px",
+                }}
+              ></div>
+              <div
+                style={{
+                  border: "1px solid",
+                  height: "10px",
+                  width: "130px",
+                  marginLeft: "10px",
+                  position: "absolute",
+                  top: "320px",
+                  left: "25px",
+                }}
+              ></div>
+              <div
+                style={{
+                  borderRadius: "50%",
+                  border: "2px solid",
+                  height: "10px",
+                  width: "10px",
+                  marginLeft: "10px",
+                  position: "absolute",
+                  top: "100px",
+                  fontSize: "8px",
+                  textAlign: "center",
+                }}
               >
-                Confirm Booking
-              </Button>
-            </Box>
-          </>
-        )}
+                <span>/.\</span>
+              </div>
+
+              {seatsRight.map((no) => (
+                <button
+                  id={no}
+                  style={{
+                    backgroundColor: "white",
+                    display: "block",
+                    marginTop: "10px",
+                    borderRadius: "5px",
+                  }}
+                  onClick={() => handleSeats(no)}
+                >
+                  {no}
+                </button>
+              ))}
+            </div>
+          </Box>
+        </Box>
       </div>
       <Snackbar
         sx={{ color: "green" }}
