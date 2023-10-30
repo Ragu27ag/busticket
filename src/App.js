6e2f131f7a67dd3d92d620dc7eb6957125ff52ce
RAGHUNATH A.G <119778016+Ragu27ag@users.noreply.github.com>
2023-10-31 00:43:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import Register from "./Components/Register";
 import { Provider } from "react-redux";
 import store from "./Store/Store.js";
 import Book from "./Components/Book";
-import BookingHistory from "./Components/BookingHistory";
-
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 // dotenv.config();
 
+const stripePromise = loadStripe(
+  "pk_test_51O6yhlSJBU0eXMnJvsIYest6gjdbRXOfuq7UlmJnL3zW86DsyksROCjVMEQLau6KVDau4v6tAQwOrOAeNOwjKQl900jbqoi2Fy"
+);
 function App() {
   // dotenv.config();
   return (
@@ -18,7 +21,14 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/book" element={<Book />} />
+                 <Route
+            path="/book"
+            element={
+              <Elements stripe={stripePromise}>
+                <Book />
+              </Elements>
+            }
+          />
           <Route path="/history" element={<BookingHistory />} />
 
           <Route path="/" element={<Navbar />} />
